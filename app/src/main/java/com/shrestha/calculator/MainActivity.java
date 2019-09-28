f1620d5ba878def5c3ff40c276aaae85d6f4528f
LastWord007 <gauravshrestha202@gmail.com>
2019-09-28 20:48:49 +0545

    new patch 1.2

--- app/src/main/java/com/shrestha/calculator/MainActivity.java
+++ app/src/main/java/com/shrestha/calculator/MainActivity.java
@@ -14,7 +14,7 @@ import com.shrestha.calculator.model.Numbers;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView result;
-    Button but_1, but_2, but_3, but_4, but_5, but_6, but_7, but_8, but_9, but_p, but_s, but_m, but_d, but_e, but_dot, but_0, but_c;
+    Button but_1, but_2, but_3, but_4, but_5, but_6, but_7, but_8, but_9, but_p, but_s, but_m, but_d, but_e, but_dot, but_0, but_c,but_del;
     int count = 0;
     String doa;
     Numbers numbers = new Numbers();
@@ -23,6 +23,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        but_del=findViewById(R.id.delete);
+        but_del.setOnClickListener(this);
         result = findViewById(R.id.result);
         result.setShowSoftInputOnFocus(false);
         but_0 = findViewById(R.id.but0);
@@ -107,34 +109,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     String NumberEntered = result.getText().toString();
                     NumberEntered = NumberEntered.substring(0, NumberEntered.length() - 1);
                     String word = (NumberEntered);
-                    result.setText("");
                     result.setText(word);
+
                     return;
 
                 } catch (Exception e) {
-                    count = 0;
+
                     result.setError("please first enter value");
                 }
                 break;
             case R.id.butadd:
                 count++;
-                doa = "plus";
+                if (count<2){
+
                 SetData(count);
+                doa = "plus";}
+                else{
+                    result.setError("only two time");
+                }
+
                 break;
             case R.id.butsub:
                 count++;
-                doa = "sub";
+                if (count<2){
+
                 SetData(count);
+                doa = "sub";}
+                else{
+                    result.setError("only two time");
+                }
                 break;
             case R.id.butdi:
                 count++;
-                doa = "di";
+                if (count<2){
+
                 SetData(count);
+                doa = "di";}
+                else{
+                    result.setError("only two time");
+                }
+
                 break;
             case R.id.butx:
                 count++;
-                doa = "mul";
+                if (count<2){
+
                 SetData(count);
+                doa = "mul";}
+                else{
+                    result.setError("only two time");
+                }
+
                 break;
             case R.id.butequal:
                 MathCal mathCal = new MathCal();
@@ -142,14 +167,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 SetData(count);
                 if (doa.equalsIgnoreCase("plus")) {
                     result.setText(mathCal.add(numbers));
+
                 } else if (doa.equalsIgnoreCase("sub")) {
                     result.setText(mathCal.sub(numbers));
+
                 } else if (doa.equalsIgnoreCase("di")) {
                     result.setText(mathCal.divide(numbers));
+
                 } else if (doa.equalsIgnoreCase("mul")) {
                     result.setText(mathCal.mul(numbers));
+
                 }
+
                 break;
+            case R.id.delete:
+                result.setText("");
+                count=0;
         }
     }
 
@@ -159,16 +192,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (cou == 1) {
                 numbers.setFirstNumber(Double.parseDouble(result.getText().toString()));
                 result.setText("");
+                return;
             } else if (cou == 2) {
                 numbers.setSecondNumber(Double.parseDouble(result.getText().toString()));
                 result.setText("");
+                return;
             } else {
                 result.setError("only two time");
-
+                return;
             }
         } else {
             result.setError("please enter the number first");
-            count = 0;
+            count--;
+            return;
+
         }
 
     }
--- app/src/main/java/com/shrestha/calculator/action/MathCal.java
+++ app/src/main/java/com/shrestha/calculator/action/MathCal.java
@@ -2,29 +2,40 @@ package com.shrestha.calculator.action;
 
 
 
-import com.shrestha.calculator.MainActivity;
 import com.shrestha.calculator.model.Numbers;
 
 public class MathCal implements Cal {
-    MainActivity ma = new MainActivity();
+
     @Override
     public String add(Numbers n) {
-//        Toast.makeText(ma, (String.valueOf((n.getFirstNumber())+(n.getSecondNumber()))), Toast.LENGTH_SHORT).show();
-        return (String.valueOf((n.getFirstNumber())+(n.getSecondNumber())));
+        String add= (String.valueOf((n.getFirstNumber())+(n.getSecondNumber())));
+//        n.setFirstNumber(0);
+//        n.setSecondNumber(0);
+        return add;
+
     }
 
     @Override
     public String sub(Numbers n) {
-        return (String.valueOf((n.getFirstNumber())-(n.getSecondNumber())));
+        String add= (String.valueOf((n.getFirstNumber())-(n.getSecondNumber())));
+//        n.setFirstNumber(0);
+//        n.setSecondNumber(0);
+        return add;
     }
 
     @Override
     public String divide(Numbers n) {
-        return (String.valueOf((n.getFirstNumber())/(n.getSecondNumber())));
+        String add= (String.valueOf((n.getFirstNumber())/(n.getSecondNumber())));
+//        n.setFirstNumber(0);
+//        n.setSecondNumber(0);
+        return add;
     }
 
     @Override
     public String mul(Numbers n) {
-        return (String.valueOf((n.getFirstNumber())*(n.getSecondNumber())));
+        String add= (String.valueOf((n.getFirstNumber())*(n.getSecondNumber())));
+//        n.setFirstNumber(0);
+//        n.setSecondNumber(0);
+        return add;
     }
 }
