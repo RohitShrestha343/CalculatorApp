a0717e4a497e93f472f4c952853169fd1a96d906
LastWord007 <gauravshrestha202@gmail.com>
2019-09-28 20:50:31 +0545

    new patch 1.3

--- app/src/main/java/com/shrestha/calculator/action/MathCal.java
+++ app/src/main/java/com/shrestha/calculator/action/MathCal.java
@@ -9,8 +9,7 @@ public class MathCal implements Cal {
     @Override
     public String add(Numbers n) {
         String add= (String.valueOf((n.getFirstNumber())+(n.getSecondNumber())));
-//        n.setFirstNumber(0);
-//        n.setSecondNumber(0);
+
         return add;
 
     }
@@ -18,24 +17,20 @@ public class MathCal implements Cal {
     @Override
     public String sub(Numbers n) {
         String add= (String.valueOf((n.getFirstNumber())-(n.getSecondNumber())));
-//        n.setFirstNumber(0);
-//        n.setSecondNumber(0);
         return add;
     }
 
     @Override
     public String divide(Numbers n) {
         String add= (String.valueOf((n.getFirstNumber())/(n.getSecondNumber())));
-//        n.setFirstNumber(0);
-//        n.setSecondNumber(0);
+
         return add;
     }
 
     @Override
     public String mul(Numbers n) {
         String add= (String.valueOf((n.getFirstNumber())*(n.getSecondNumber())));
-//        n.setFirstNumber(0);
-//        n.setSecondNumber(0);
+
         return add;
     }
 }
